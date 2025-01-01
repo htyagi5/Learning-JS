b5ec9ab390f8182b2f160a94ed6701deebb109a1
htyagi5 <harshit2310079@akgec.ac.in>
2025-01-01 12:04:48 +0530

    new project imageReels

---
+++ 07_projects/Project8/chaiaurjs.js
@@ -0,0 +1,36 @@
+const img=document.querySelector(".content img")
+const baseurl='https://picsum.photos'
+const btn=document.querySelector('button')
+const randomno=()=>{
+    let number='0123456789'
+    let Random=Math.floor(Math.random(number)*1000+500)
+    if(Random>900){
+    return Random
+    }
+    else{
+        Random=900
+        return Random
+    }
+    // console.log(Random)
+}
+btn.addEventListener("click",()=>{
+    setInterval(function(){
+        img.src= `${baseurl}/${randomno()}/${randomno()}`
+         console.log(img)
+         },4000)
+})
+
+
+
+
+// const img=document.querySelector(".content")
+// const baseurl='img'
+// const btn=document.querySelector('button')
+// const end='.jpeg'
+// let i=1;
+// btn.addEventListener("click",()=>{
+//     setInterval(function(){
+//         img.innerHTML= `<img src="${baseurl}${i}${end}" alt="img"></img>`
+//         i++;
+//          },100)
+// })
