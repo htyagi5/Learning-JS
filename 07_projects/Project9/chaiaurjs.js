905122a3cd20786ef363973ef354ffa27226fc2e
htyagi5 <harshit2310079@akgec.ac.in>
2025-01-06 21:51:11 +0530

    Calculator simple and smooth

---
+++ 07_projects/Project9/chaiaurjs.js
@@ -0,0 +1,48 @@
+let input=document.querySelector('.input')
+let number=document.querySelectorAll('.numbers')
+let operator=document.querySelectorAll('.operators')
+let spkeys=document.querySelectorAll('.roler')
+input.ariaPlaceholder='0'
+let prev=0;
+number.forEach((key)=>{
+    key.addEventListener('click',(e)=>{
+        // prev=input.innerText
+       input.innerText+=Number(e.target.id)
+    //    return prev
+    })
+})
+operator.forEach((key)=>{
+    key.addEventListener('click',(e)=>{
+        if(e.target.id!='C'){
+        // prev=input.innerText
+       input.innerText+=e.target.id
+    //    return prev
+    //    console.log('click')
+        }
+        else{
+            input.innerText='0'
+        }
+    })
+})
+let result=''
+spkeys.forEach((key)=>{
+    key.addEventListener('click',(e)=>{
+        if(e.target.id=='equal'){
+        try{
+            result=eval(input.innerText)
+            input.innerText=`${result}`
+            // console.log(result)
+        }
+        catch{
+        input.innerText="error";
+        }
+    }
+    else if(e.target.id=='cut'){
+        input.innerText=input.innerText.slice(0,-1) || '0';
+    }
+    // else{
+    //      input.innerText=prev
+    // }
+    })
+})
+
